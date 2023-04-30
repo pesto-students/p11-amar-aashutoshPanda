9ab08d096ea1528cf93c5290c06b4fe98a669d9e
ashutoshPanda <iamashutoshpanda@gmail.com>
2023-04-30 13:35:19 +0530

    Add: 2 & 3

--- Week-4/Session-09_JS_Basics/Assignments/JavaScript game object/script.js
+++ Week-4/Session-09_JS_Basics/Assignments/JavaScript game object/script.js
@@ -1,17 +1,35 @@
 // Your solution goes here
+class Game {
+  constructor() {
+    this.lives = 3;
+    this.coins = 0;
+  }
 
+  get points() {
+    return this.coins * 10;
+  }
 
-console.log("lives = " + game.lives);    // should be 3
-console.log("coins = " + game.coins);    // should be 0
-console.log("points = " + game.points);  // should be 0
+  playerDies() {
+    this.lives = Math.max(0, this.lives - 1);
+  }
+  newGame() {
+    this.lives = 3;
+    this.coins = 0;
+  }
+}
+
+const game = new Game();
+console.log("lives = " + game.lives); // should be 3
+console.log("coins = " + game.coins); // should be 0
+console.log("points = " + game.points); // should be 0
 game.coins = 2;
-console.log("points = " + game.points);  // should be 20
+console.log("points = " + game.points); // should be 20
 game.playerDies();
-console.log("lives = " + game.lives);    // should be 2
+console.log("lives = " + game.lives); // should be 2
 game.playerDies();
 game.playerDies();
 game.playerDies();
-console.log("lives = " + game.lives);    // should be 0
+console.log("lives = " + game.lives); // should be 0
 game.newGame();
-console.log("lives = " + game.lives);    // should be 3
-console.log("coins = " + game.coins);    // should be 0
+console.log("lives = " + game.lives); // should be 3
+console.log("coins = " + game.coins); // should be 0
--- Week-4/Session-09_JS_Basics/Assignments/JavaScript number guessing game/script.js
+++ Week-4/Session-09_JS_Basics/Assignments/JavaScript number guessing game/script.js
@@ -1,1 +1,32 @@
-// Your solution goes here 
+function playGuessingGame(numToGuess, totalGuesses = 10) {
+  let guessCount = 0;
+  let guess;
+
+  while (guessCount < totalGuesses) {
+    guess = prompt(
+      guessCount === 0
+        ? "Enter a number between 1 and 100."
+        : guess < numToGuess
+        ? `${guess} is too small. Guess a larger number.`
+        : `${guess} is too large. Guess a smaller number.`
+    );
+
+    if (guess === null) {
+      return 0;
+    }
+
+    if (isNaN(Number(guess)) || guess.trim() === "") {
+      alert("Please enter a number.");
+      continue;
+    }
+
+    guess = Number(guess);
+    guessCount++;
+
+    if (guess === numToGuess) {
+      return guessCount;
+    }
+  }
+
+  return 0;
+}
