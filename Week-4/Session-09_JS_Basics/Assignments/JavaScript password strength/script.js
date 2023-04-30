3da3a082c7b4aeb51f76f102c77bc79777d2dc9d
ashutoshPanda <iamashutoshpanda@gmail.com>
2023-04-30 18:44:01 +0530

    ADD: solution closure

--- Week-4/Session-09_JS_Basics/Assignments/JavaScript password strength/script.js
+++ Week-4/Session-09_JS_Basics/Assignments/JavaScript password strength/script.js
@@ -1,1 +1,24 @@
-// Your solution goes here 
+function isStrongPassword(password) {
+  // Check if password is at least 8 characters long
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Check if password contains "password" string
+  if (password.toLowerCase().includes("password")) {
+    return false;
+  }
+
+  // Check if password contains at least one uppercase character
+  if (password.toLowerCase() === password) {
+    return false;
+  }
+
+  // If all conditions pass, return true
+  return true;
+}
+
+console.log(isStrongPassword("Qwerty")); // false - Too short
+console.log(isStrongPassword("passwordQwerty")); // false - Contains "password"
+console.log(isStrongPassword("qwerty123")); // false - No uppercase characters
+console.log(isStrongPassword("Qwerty123")); // true
