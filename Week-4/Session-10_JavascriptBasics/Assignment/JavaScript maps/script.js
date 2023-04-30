d31e966e866ffac5eaebe01e12cad51f90bf90f1
ashutoshPanda <iamashutoshpanda@gmail.com>
2023-04-30 19:15:53 +0530

    Add: map

--- Week-4/Session-10_JavascriptBasics/Assignment/JavaScript maps/script.js
+++ Week-4/Session-10_JavascriptBasics/Assignment/JavaScript maps/script.js
@@ -1,3 +1,17 @@
+"use strict";
+
 function calcWordFrequencies() {
-   
+  const input = prompt();
+  const counts = {};
+  console.log(input.split(" "));
+  input
+    .split(" ")
+    .forEach((ele) =>
+      counts.hasOwnProperty(ele) ? (counts[ele] += 1) : (counts[ele] = 1)
+    );
+  for (const [key, val] of Object.entries(counts)) {
+    console.log(key, val);
+  }
 }
+
+calcWordFrequencies();
